319471baa03f232d89e9888cb8e4c14e0b39ffca
hathaikarn sitthipan <sweetypink9@gmail.com>
2014-01-29 11:37:20 +0700

    Edited by hathaikarn

--- Lab11_1.php
+++ Lab11_1.php
@@ -1,8 +1,6 @@
 <?php
 
                phpinfo();
-               echo '<p>Hello World</p>';
-               echo'<p>What awonderful day</p>';
-               echo'<p>We are having fun</p>';
-               
+               echo '<p>My name is Hathaikarn</p>';
+                     
 ?>
